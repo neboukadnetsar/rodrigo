f98c4b98b075fe045aa00f1f7b391a7ee876d3c0
Gabriel D'Amour <A22.gabriel.damour@cegepsherbrooke.qc.ca>
2023-07-19 18:06:10 -0400

    test

--- queries/DBPool.js
+++ queries/DBPool.js
@@ -2,4 +2,9 @@ const Pool = require('pg').Pool;
 
 // const dbConfig = require('../db_config');
 
-module.exports = new Pool(process.env.dbConfig);
+module.exports = new Pool({
+    connectionString: process.env.DBConfigLink,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
